e99139d164a4012564af444908bf5e8c420942a6
Ulden <manjuulden@hotmail.com>
2015-11-25 04:41:06 +0800

    adjustment

--- database.php
+++ database.php
@@ -18,7 +18,7 @@ class database{
 		$this -> connect = NULL;
 	}
 	public function connect_to_db(){
-		$dns = 'mysql:host='.$this->mysql_host.":".$this->mysql_port.";dbname=".$this->mysql_db;
+		$dns = 'mysql:host='.$this->mysql_host.";port=".$this->mysql_port.";dbname=".$this->mysql_db;
 		try{
 			$this->connect = new PDO($dns, $this->mysql_user,$this->mysql_password);
 		}
