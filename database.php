89ce85edfb8e50679588aec18a0ec6564ce3c340
Ulden <manjuulden@hotmail.com>
2015-11-25 03:12:47 +0800

    input into db is done

---
+++ database.php
@@ -0,0 +1,74 @@
+<?php
+class DATABASE{
+	private $mysql_db,
+			$mysql_host,
+			$mysql_password,
+			$mysql_port,
+			$mysql_user,
+			$connect;
+	function __construct(){
+		$mysql_host = SAE_MYSQL_HOST_M;
+		$mysql_db = SAE_MYSQL_DB;
+		$mysql_user = SAE_MYSQL_USER;
+		$mysql_password = SAE_MYSQL_PASS;
+		$mysql_port = SAE_MYSQL_PORT;
+		connect_to_db();
+	}
+	function __destruct(){
+		close_connection();
+	}
+	function connect_to_db(){
+		$connect = mysql_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db);
+		if(!$connect) die ("Unable to connect to MySQL: ".mysql_error());
+	}
+	function close_connection(){
+		mysql_close($connect);
+	}
+	function operation_fail($query){
+		if(!mysql_query($query,$connect)){
+			echo "insert into user failed: $query<br />".mysql_error()."<br /><br />";
+		}
+	}
+	function put_user_in_db($id,$name,$password,$email,$role,$id_card,$tel,$status){
+		$query = "INSERT INTO users VALUES".
+		"('$id','$name','$password','$email','$role','$id_card','$tel','$status')";
+		operation_fail($query);
+	}
+	function put_city_in_db($id,$name,$provinces){
+		$query = "INSERT INTO city VALUES"."('$id','$name','$provinces')";
+		operation_fail($query);
+	}
+	function put_grade_in_db($id,$detail){
+		$query = "INSERT INTO grade VALUES"."('$id','$detail')";
+		operation_fail($query);
+	}
+	function put_major_in_db($id,$name,$intro){
+		$query = "INSERT INTO major VALUES"."('$id','$name','$intro')";
+		operation_fail($query);
+	}
+	function put_hospital_in_db($id,$name,$city,$address,$major_id,$grade_id,$tel,$intro){
+		$query = "INSERT INTO hospital VALUES".
+			"('$id','$name','$city','$address','$major_id','$grade_id','$tel','$intro')";
+			operation_fail($query);
+	}
+	function put_department_in_db($id,$name,$hospital_id,$intro){
+		$query = "INSERT INTO department VALUES"."('$id','$name','$hospital_id','$intro')";
+		operation_fail($query);
+	}
+	function put_doctor_in_db($id,$name,$department_id,$major_id,$grade_id,$hospital_id,$intro,$calendar_id){
+		$query = "INSERT INTO department VALUES"."('$id','$name','$department_id','$major_id','$grade_id','$hospital_id','$intro','$calendar_id')";
+		operation_fail($query);
+	}
+	function put_calendar_in_db($id,$type,$detail,$off_start,$off_end){
+		$query = "INSERT INTO calendar VALUES".
+			"'$id','$type','$detail','$off_start','$off_end')";
+		operation_fail($query);
+	}
+	function put_order_hospital_in_db($id,$user_id,$doctor_id,$order_date,$order_time,$order_status){
+		$query = "INSERT INTO order_hospital VALUES".
+			"'$id','$user_id','$doctor_id','$order_date','$order_time','$order_status')";
+		operation_fail($query);
+	}
+}
+
+?>
---
+++ hospital.sql
@@ -0,0 +1,93 @@
+create table users(
+id int,
+name varchar(50),
+password varchar(50),
+email varchar(50),
+role int,
+id_card char(18),
+tel char(11),
+status int,
+create_time timestamp,
+update_time timestamp
+);
+create table calendar(
+id int primary key,
+type int,
+detail varchar(255),
+off_start date,
+off_end date,
+create_time timestamp,
+update_time timestamp
+);
+create table city(
+id int primary key,
+name varchar(50),
+provinces varchar(50),
+create_time timestamp,
+update_time timestamp
+);
+create table major(
+id int primary key,
+name varchar(50),
+intro text,
+create_time timestamp,
+update_time timestamp
+);
+create table grade(
+id int primary key,
+detail varchar(50),
+create_time timestamp,
+update_time timestamp
+);
+create table hospital(
+id int primary key,
+name varchar(50),
+city int,
+address text,
+major_id int,
+grade_id int,
+tel char(11),
+intro text,
+create_time timestamp,
+update_time timestamp,
+foreign key (city) references city(id),
+foreign key (grade_id) references grade(id)
+);
+create table department(
+id int primary key,
+name varchar(50),
+hospital_id int,
+intro text,
+create_time timestamp,
+update_time timestamp,
+foreign key (hospital_id) references hospital(id)
+);
+create table doctor(
+id int primary key,
+name varchar(20),
+department_id int,
+major_id int,
+grade_id int,
+hospital_id int,
+intro text,
+calendar_id int,
+create_time timestamp,
+update_time timestamp,
+foreign key (department_id) references department(id),
+foreign key (major_id) references major(id),
+foreign key (grade_id) references grade(id),
+foreign key (hospital_id) references hospital(id),
+foreign key (calendar_id) references calendar(id)
+);
+create table order_hospital(
+id int primary key,
+user_id int,
+doctor_id int,
+order_date Date,
+order_time time,
+order_status int,
+create_time timestamp,
+update_time timestamp,
+foreign key (user_id) references users(id),
+foreign key (doctor_id) references doctor(id)
+);
