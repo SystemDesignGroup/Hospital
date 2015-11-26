c7ddce1bf22bab869440f00d37365e6e1ede81c2
Ulden <manjuulden@hotmail.com>
2015-11-26 13:07:59 +0800

    解决了存数据错误的问题

--- database.php
+++ database.php
@@ -28,41 +28,46 @@ class database{
 		}
 	}
 	public function put_user_in_db($id,$name,$password,$email,$role,$id_card,$tel,$status){
-		$this->connect->exec("INSERT INTO users VALUES".
+		$this->connect->exec("INSERT INTO users(id,name,password,email,role,id_card,tel,status) VALUES".
 		"('$id','$name','$password','$email','$role','$id_card','$tel','$status')");
 		
 	}
 	public function put_city_in_db($id,$name,$provinces){
-		$this->connect->exec("INSERT INTO city VALUES"."('$id','$name','$provinces')");
+		$this->connect->exec("INSERT INTO city(id,name,provinces) VALUES".
+		"('$id','$name','$provinces')");
 	}
 	public function put_grade_in_db($id,$detail){
-		$this->connect->exec("INSERT INTO grade VALUES"."('$id','$detail')");
+		$this->connect->exec("INSERT INTO grade(id,detail) VALUES".
+		"('$id','$detail')");
 		
 	}
 	public function put_major_in_db($id,$name,$intro){
-		$this->connect->exec( "INSERT INTO major VALUES"."('$id','$name','$intro')");
+		$this->connect->exec( "INSERT INTO major(id,name,intro) VALUES".
+		"('$id','$name','$intro')");
 		
 	}
 	public function put_hospital_in_db($id,$name,$city,$address,$major_id,$grade_id,$tel,$intro){
-		$this->connect->exec( "INSERT INTO hospital VALUES".
+		$this->connect->exec( "INSERT INTO hospital(id,name,city,address,major_id,grade_id,tel,intro) VALUES".
 			"('$id','$name','$city','$address','$major_id','$grade_id','$tel','$intro')");
 			
 	}
 	public function put_department_in_db($id,$name,$hospital_id,$intro){
-		$this->connect->exec( "INSERT INTO department VALUES"."('$id','$name','$hospital_id','$intro')");
+		$this->connect->exec( "INSERT INTO department(id,name,hospital_id,intro) VALUES".
+		"('$id','$name','$hospital_id','$intro')");
 		
 	}
 	public function put_doctor_in_db($id,$name,$department_id,$major_id,$grade_id,$hospital_id,$intro,$calendar_id){
-		$this->connect->exec( "INSERT INTO department VALUES"."('$id','$name','$department_id','$major_id','$grade_id','$hospital_id','$intro','$calendar_id')");
+		$this->connect->exec( "INSERT INTO department(id,name,department_id,major_id,grade_id,hospital_id,intro,calendar_id) VALUES".
+		"('$id','$name','$department_id','$major_id','$grade_id','$hospital_id','$intro','$calendar_id')");
 		
 	}
 	public function put_calendar_in_db($id,$type,$detail,$off_start,$off_end){
-		$this->connect->exec( "INSERT INTO calendar VALUES".
+		$this->connect->exec( "INSERT INTO calendar(id,type,detail,off_start,off_end) VALUES".
 			"'$id','$type','$detail','$off_start','$off_end')");
 		
 	}
 	public function put_order_hospital_in_db($id,$user_id,$doctor_id,$order_date,$order_time,$order_status){
-		$this->connect->exec( "INSERT INTO order_hospital VALUES".
+		$this->connect->exec( "INSERT INTO order_hospital(id,user_id,doctor_id,order_date,order_time,order_status) VALUES".
 			"'$id','$user_id','$doctor_id','$order_date','$order_time','$order_status')");
 		
 	}
