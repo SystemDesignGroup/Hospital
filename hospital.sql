525c5e4ae6663fa782a612d2f9276fbac08c89e9
Ulden <manjuulden@hotmail.com>
2015-11-29 00:45:20 +0800

    注释写错了

--- hospital.sql
+++ hospital.sql
@@ -1,4 +1,4 @@
-//本地调试时，最后一张表可能会报错errorno:150，这个是mysql本身的bug，不用在意。在部署环境中确定没有问题。
+#本地调试时，最后一张表可能会报错errorno:150，这个是mysql本身的bug，不用在意。在部署环境中确定没有问题。
 create database hospital;
 use hospital;
 create table users(
