994e0b265ded6879bb77712415cd6132c1c7fe86
Ulden <manjuulden@hotmail.com>
2015-11-29 00:23:31 +0800

    adjustment

--- database.php
+++ database.php
@@ -54,9 +54,9 @@ class database{
 			default: echo "No Such Table In This Data Base<br />";
 		}
 	}
-	public function get_field_from_table($table,$field,$key,$key_field){//表名，待查询字段（需要的结果），查询依据的字段，查询所依据字段值
+	public function get_field_from_table($table,$field,$key_field,$key){//表名，待查询字段（需要的结果），查询依据的字段，查询所依据字段值
 		$result = $this->connect->query("SELECT $field FROM $table WHERE $key_field='$key'");
-		$var = $result->fetch();
+		$var = $result->fetchColumn();
 		return $var;
 	}
 	
