6d127f48a9452c1afe067ce604a38d281aaf81da
shtb <13240333607@163.com>
2015-12-02 16:54:16 +0800

    login system for admin,(管理员登陆页面鼠标放在登录按钮上时会有很丑的滑块，我无法解决)

---
+++ admin/admin_login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+	<?php 
+	$html_fail = <<<HTML
+	<head>
+	 	<meta charset="UTF-8"/>
+	 	<title>管理员登录</title>
+		 <link type="text/css" rel="stylesheet" href="../css/admin.css"/>
+	</head>
+		<div id="head_div"> 
+			<span>欢迎来到全国统一挂号平台</span>
+		</div>
+			<center>
+			<div>
+				<font size="5"  color="red">登录失败，您是否选择了错误的登录入口?<br>（普通用户请在首页点击“登录”而不是“管理员登录”）</font>
+			</div>
+			<br>
+			<div class="normal-btn">
+				<a href="../home.html">返回首页</a>
+			</div>
+			</center>
+HTML;
+	$html_success = <<<HTML
+	<meta http-equiv="refresh" content="2; url=editHospital.html"> 
+<head>
+	 	<meta charset="UTF-8"/>
+	 	<title>管理员登录</title>
+		 <link type="text/css" rel="stylesheet" href="../css/admin.css"/>
+	</head>
+		<div id="head_div"> 
+			<span>欢迎来到全国统一挂号平台</span>
+		</div>
+	<center>
+	<div>
+		<font size="5"  color="gray">登录成功，即将跳转到管理员系统...</font>
+	</div>
+	</center>
+HTML;
+		$hname=$_POST['user'];
+		if(strlen($hname)<1)
+		{
+			echo $html_fail;			
+		}
+		else
+		{
+			echo $html_success;
+		}
+?> 
+
+</html>
