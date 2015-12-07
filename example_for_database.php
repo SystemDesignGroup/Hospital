2e84063dbdd79accd194ced0cceb7df4d34d30e9
Ulden <manjuulden@hotmail.com>
2015-12-07 22:00:54 +0800

    new edition of database

--- example_for_database.php
+++ example_for_database.php
@@ -6,11 +6,7 @@ $values=array(
 	'name' => "GuanFuHe",
 	'password' => "123456"
 );
-#$db->insert_data_into_table('users',$values);
-
-$key_values=array(
-	'province'=>"北京市"
-);
+$db->insert_data_into_table('users',$values);
 print_r($key_values);
 $results = $db->get_field_from_table('city','*',$key_values);
 print_r($results);
