5698ac678e3f5249c490f0cd64f87f40ce06031d
wq <740831268@qq.com>
2015-12-11 05:46:11 -0800

    register2.php

--- register.php
+++ register.php
@@ -6,24 +6,25 @@
  * Time: 1:46 AM
  */
 
-require_once '../database.php';
+    require_once "database.php";
+if(isset($_POST["submit"]) && $_POST["submit"] == "注册") {
+    $email = $_POST['email'];
+    $username = $_POST['name'];
+    $tel = $_POST['tel'];
+    $card = $_POST['id_card'];
+    $password = $_POST['password'];
 
-$email = $_POST['user_email'];
-$username = $_POST['user_name'];
-$tel = $_POST['user_tel'];
-$card = $_POST['user_card'];
-$password = $_POST['user_password'];
-
-$succeed = true;
-
-if($succeed) {
-
+    //连接数据库
     $db = new database();
     $db->connect_to_db();
 
     //insert
-    $user_re = $db ->get_field_from_table('users','id','id_card',$card);
-    if(count($user_re) == 0) {
+    $user_re = $db->get_field_from_table('users', 'id', 'id_card');
+    if (count($user_re) != 0) //如果已经存在该用户
+    {
+        echo "<script>alert('用户名已存在'); history.go(-1);</script>";
+    } else //该用户不存在
+    {
         $values = array(
             'email' => $email,
             'name' => $username,
@@ -31,22 +32,22 @@ if($succeed) {
             'id_card' => $card,
             'password' => $password
         );
-        $db->insert_data_into_table('users',$values);
-        echo "你的用户";
-        echo $username;
-        echo "已成功注册！";
-
-
+        $db->insert_data_into_table('users', $values);
+        echo "<script>alert('注册成功！'); history.go(-1);</script>";
     }
-
-    else{
-        echo "注册失败！";
-    }
-
-
-
-
-
-
-
 }
+
+else {
+    echo "<script>alert('提交未成功！'); history.go(-1);</script>";
+}
+
+?>
+
+
+
+
+
+
+
+
+
