538323ac548cd15194c430f111ebc5202f813081
997962520 <997962520@qq.com>
2015-12-13 10:53:48 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,32 @@
 <?php
 //登录
 require_once '../database.php';
-
-$username = $_POST['username'];
+$username = $_POST['user'];
 $password = $_POST['password'];
 
-$errmsg='';
-if(!empty($errmsg))  //yan zheng shu ju wei kong
+session_start();
+if(empty($username) or empty($password))
 {
-    if(empty($username))
-        $errmsg="数据输入不完整";
-        echo($errmsg);
+    echo "数据输入不完整!";
+    header("Location:login.html");
 }
-if(empty($errmsg))
+else
 {
     $db=new database();
     $db->connect_to_db();
     if(mysqli_connect_errno())
     {
-        $errmsg="shu ju ku lian jie shi bai!<br>\n";
-        echo($errmsg);
+        echo"shu ju ku lian jie shi bai!";
     }
     else
     {
-        $key_username=array('name'=>$username);
-        $key_password=array('password'=>$username);
+        $key_username=array('users.name'=>$username);
+        $key_password=array('users.password'=>$username);
         $realuname=$db->get_field_from_table('users','users.name',$key_username);
         $realpwd=$db->get_field_from_table('users','users.password',$key_password);
         if(count($key_username)==0)
         {
-            echo "用户不存在";
+            echo"用户不存在!";
         }
         else
         {
@@ -37,21 +34,17 @@ if(empty($errmsg))
             $realpassword=$realpwd[0]['password'];
             if($username==$realusername && $password==$realpassword)
             {
-                $errmsg="deng lu cheng gong";
-                echo($errmsg);
-                header("refresh:0;url=home.html");
-                session_start();
+                echo"登录成功!";
                 $_SESSION['uid']=$username;
                 if(empty($_SESSION['uid']))
                 {
-                    echo"你还没有登录";
+                    echo "<script>alert('你还没有登');</script>";
                 }
                 exit;
             }
             else
             {
-                $errmsg="deng lu shi bai!";
-                echo($errmsg);
+                echo"登录失败！";
                 exit;
             }
         }
