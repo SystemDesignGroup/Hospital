300f1e7376169e294a9cb38ab77c91e54fd5dc44
shtb <13240333607@163.com>
2015-12-13 18:52:32 +0800

    admin_login system finished

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,34 +1,9 @@
 <!DOCTYPE html>
-
 <html>
 	<?php 
-	$html_fail = <<<HTML
-	<head>
-	 	<meta charset="UTF-8"/>
-	 	<title>管理员登录</title>
-		 <link type="text/css" rel="stylesheet" href="../css/admin.css"/>
-	</head>
-		<div id="head_div"> 
-			<span>欢迎来到全国统一挂号平台管理中心</span>
-		</div>
-		<center>
-		<div id="logo_div">
-			<div id="logo-title">
-				<img src="../image/small_logo.jpg" alt="挂号平台logo" id="logo"/>
-				<h1>全国统一挂号平台管理中心</h1>				
-			</div>
-		</div>	
-		<div>
-			<font size="5"  color="red">登录失败，您是否选择了错误的登录入口?<br>（普通用户请在首页点击“登录”而不是“管理员登录”）</font>
-		</div>
-		<br>
-		<div class="normal-btn">
-			<a href="../home.html">返回首页</a>
-		</div>
-		</center>
-HTML;
+	
 	$html_success = <<<HTML
-	<meta http-equiv="refresh" content="2; url=editHospital.html"> 
+	<meta http-equiv="refresh" content="1; url=editHospital.html"> 
 <head>
 	 	<meta charset="UTF-8"/>
 	 	<title>管理员登录</title>
@@ -49,15 +24,36 @@ HTML;
 	</div>
 	</center>
 HTML;
-		$hname=$_POST['user'];
-		if(strlen($hname)<1)
-		{
-			echo $html_fail;			
-		}
-		else
+
+	require_once '../database.php';
+	$name=$_POST['user'];
+	$password=$_POST['password'];
+	$db=new database();
+	$db->connect_to_db();
+	if($name === "")
+	{
+		echo"<script type='text/javascript'>alert('请填写用户名');location='../login_for_admin.html';
+            </script>";
+	}
+	elseif($password === "")
+	{
+		echo"<script type='text/javascript'>alert('请填写密码');location='../login_for_admin.html';</script>";
+	}
+	else
+	{
+		$hos_values=array(
+			'name' => $name,
+			'password'=>$password
+			);
+		$hospital_id=0;
+		$hos_re=$db->get_field_from_table('users','role',$hos_values);
+		if($hos_re[0][role]==2)
 		{
 			echo $html_success;
 		}
-?> 
+		else
+			echo "<script type='text/javascript'>alert('登录失败，您是否选择了错误的登录入口?（普通用户请在首页点击“登录”而不是“管理员登录”）');location='../login_for_admin.html';</script>";
+	}
+	?>
 
-</html>
+</html> 
