5756a15240037e32f48694dcc44b5ff8a2b3c597
xjf13211016 <349745687@qq.com>
2015-12-21 08:12:14 -0800

    tian jia le an ke shi sou suo de js wen jian

--- search.php
+++ search.php
@@ -5,49 +5,29 @@
  * Date: 12/4/15
  * Time: 11:48 PM
  */
+require_once"database.php";
+
 set_time_limit(600);
-$keyword=trim($_POST[$keyword]);
-if($keyword=""){
+
+$key=$_GET['key'];
+
+if($key=""){
     echo"搜索的内容不能为空";
     exit;
 }
-//遍历文件的函数
-function lisfile($dir,$keyword,&$array){
-    $handle=opendir($dir);
-    while(false!==($file=readdir($handle))){
-        if($file!="."&&$file!=".."){
-            //若是目录则继续
-            if(is_file("$dir/$file")){
-                lisfile("$dir/$file",$keyword,$array);
-            }
-            else{//处理部分
-                //读取文件内容
-                $data=fread(fopen("$dir/$file","r"),filesize("$dir/$file"));
-                if(preg_match("/<body([^>]+)>(.+)</body>/i",$data,$b)){//只搜索网页主题部分
-                    $body=strip_tags($b["2"]);
-                    }
-                else{
-                    $body=strip_tags($data);
-                }
-                if($file!="search.php"){//不搜索自身
-                    if(preg_match("/$keyword/i",$body)){//检查匹配
-                        if(preg_match("/<title>(.+)</title>/i",$data,$m)){
-                            $title=$m["1"];
-                        }
-                        else{//如果找不到标题则记为没有标题
-                            $title="没有标题";
-                        }
-                        $array[]="$dir/$file $title";
-                    }
-                }
-            }
-        }
-    }
+
+function searchhospital($table,$field,$key,$keyfield,&$array){
+    $conn=new database();
+    $keyl=array("\'$keyfield\'"=>$key);
+    $hospital_id=$conn->get_field_from_table("$table","$field",$keyl);
+    $hospital_id_s = $hospital_id[0]['hospital_id'];
+    $keys1 = array('id'=>$hospital_id_s);
+    $array= $conn->get_field_from_table("hospital","name",$keys1);
+
 }
 //定义数组
 $array=array();
 //执行函数
-lisfile(".","$keyword",$array);
 if($array!=null) {//当array不为空时
     foreach ($array as $value) {//拆分并输出
         list($filedir, $title) = preg_split("[]", $value, 2);
---
+++ search/search_by_department.php
@@ -0,0 +1,27 @@
+<?php
+require_once'../database.php';
+//链接数据库
+$db=new database();
+$db->connect_to_db();
+
+//获取相关医院信息
+$tab=$_GET['tab'];
+$leibie=$_GET['leibie'];
+
+$text=array('name'=>$tab);
+$hospital_id=$db->get_field_from_table("department","hospital_id",$text);
+$hospital_id_s = $hospital_id[0]['hospital_id'];
+$keys1 = array('id'=>$hospital_id_s);
+$result_hos= $db->get_field_from_table("hospital","id,name,address,tel,intro",$keys1);
+//获取相关医生信息
+$department_id=$db->get_field_from_table("department","id",$text);
+$department_id_s = $department_id[0]['id'];
+$keys1 = array('department_id'=>$department_id_s);
+$result_doc= $db->get_field_from_table("doctor","id,name,grade_id,intro,tickets",$keys1);
+if($leibie=='hos'&&$result_hos!=null){
+    echo json_encode($result_hos);
+}
+if($leibie=='doc'&&$result_doc!=null){
+    echo json_encode($result_doc);
+}
+?>
