3dac547aa9cf036af1df0e7d81732de406eaa350
lyg11211016 <25424570439@qq.com>
2015-12-24 00:27:14 +0800

    yiyuan

--- js/hos_search.js
+++ js/hos_search.js
@@ -111,38 +111,40 @@ function byGrade(grade){
         if(xmlHttp.readyState == 4 && xmlHttp.status ==200){
             response=eval(xmlHttp.response);
             for(var i=0;i<response.length;i++){
-                var listItem="<li class='g-hospital-item J_hospitalItem'>"+
-                    "<div class='hos-total'>"+
-                    "<div class='info g-left' style='float:left'>"+
-                    "<a target='_blank' href='http://www.guahao.com/hospital/5cee04f9-4cc8-4499-a35b-6f37f2dd8a74000' onmousedown='return _smartlog(this,HPTP_1)' class='img' style='float:left'>"+
-                    "<img src='../image/pku_hos.jpg' alt='' title=''/>"+
-                    "</a>"+
-                    "<dl style='float:left;margin-left:0px;text-align:center'>" +
-                    "<dt>"+
-                    "<a href='' class='name'>" + response[i]['name'] + "</a>" +
-                    "<em class='em2'>"+response[i]['grade']+"</em>"+
-                    "<img src='http://img.guahao.cn/portal/img/guahao.png'>"+
-                    "</dt>"+
-                    "<dd>"+
-                    "<p class='tel'>"+
-                    "<i>"+"</i>"+
-                    "<span title=''>"+response[i]['tel']+"</span>"+
-                    "</p>"+
-                    "<p class='addr'>"+
-                    "<i>"+"</i>"+
-                    "<span title=''>"+response[i]['address']+"</span>"+
-                    "</p>"+
-                    "</dd>"+
-                    "</dl>"+
-                    "<p><label>医院简介：</label>" + response[i]['intro'] + "</p>" +
-                    "</div>"+
-                    "<div class='comment' style='float:left margin-right:920px;margin-top:200'>"+
-                    "累计 <label>133.0万</label> 次预约<div class='pd15'> "+"</div>"+
-                    "累计 <label>6.1万</label> 条患者评价"+
-                    "</div>"+
-                    "</div>"+
-                    "<a class='cover-bg' target='_blank' href='http://www.guahao.com/hospital/5cee04f9-4cc8-4499-a35b-6f37f2dd8a74000'>"+"</a>"+
-                    " </li>";
+                var listItem =
+                    "<li class='g-hospital-item J_hospitalItem'>" +
+                    "<div class='hos-total' style='width:100%;height:145px;border-bottom:1px solid #DBDBDB'>" +
+                    "<div class='info g-left' style='float:left'>" +
+                    "<a target='_blank' href='#' onmousedown='return _smartlog(this,'HPTP_1')' class='img' style='float:left'>" +
+                    "<img src='http://img.guahao.cn/images/hospital/hlarge_5cee04f9-4cc8-4499-a35b-6f37f2dd8a74.gif' alt='中国人民解放军总医院301医院' title='中国人民解放军总医院301医院' />" +
+                    "</a>" +
+                    "<dl style='float:left'>" +
+                    "<dt>" +
+                    "<a class='a' href='' onmousedown='return _smartlog(this,'HPTN_1')'target='blank' title='中国人民解放军总医院301医院'>"+response[i]['name']+"</a>" +
+                    "<em class='em2'>"+Grade+"</em>" +
+                    "<img src='http://img.guahao.cn/portal/img/guahao.png'>" +
+                    "</dt>" +
+                    "<dd>" +
+                    "<p class='tel'>" +
+                    "<i></i>" +
+                    "<span title='010-68182255'>"+response[i]['tel']+"</span>" +
+                    "</p>" +
+                    "<p class='addr'>" +
+                    "<i></i>" +
+                    "<span title='北京市海淀区复兴路28号'>"+response[i]['address']+"</span>" +
+                    "</p>" +
+
+                    "</dd>" +
+                    "</dl>" +
+                    "</div>" +
+                    "<div class='comment'style='float:right;margin-right:100px;margin-top:40px'>" +
+                    "累计 <label>133.0万</label> 次预约<div class='pd15'> </div>" +
+                    "累计 <label>6.1万</label> 条患者评价" +
+                    "</div>" +
+                    "</div>" +
+                    "</li>";
+
+                hospitallist.innerHTML += listItem;
             }
         }
     }
@@ -161,41 +163,43 @@ function byMajor(major){
 
     xmlHttp=new XMLHttpRequest();
     xmlHttp.onreadystatechange = function(){
-        if(xmlHttp.readyState == 4 && xmlHttp.status ==200){
-            response=eval(xmlHttp.response);
-            for(var i=0;i<response.length;i++){
-                var listItem="<li class='g-hospital-item J_hospitalItem'>"+
-                    "<div class='hos-total'>"+
-                    "<div class='info g-left' style='float:left'>"+
-                    "<a target='_blank' href='http://www.guahao.com/hospital/5cee04f9-4cc8-4499-a35b-6f37f2dd8a74000' onmousedown='return _smartlog(this,HPTP_1)' class='img' style='float:left'>"+
-                    "<img src='../image/pku_hos.jpg' alt='' title=''/>"+
-                    "</a>"+
-                    "<dl style='float:left;margin-left:0px;text-align:center'>" +
-                    "<dt>"+
-                    "<a href='' class='name'>" + response[i]['name'] + "</a>" +
-                    "<em class='em2'>"+response[i]['grade']+"</em>"+
-                    "<img src='http://img.guahao.cn/portal/img/guahao.png'>"+
-                    "</dt>"+
-                    "<dd>"+
-                    "<p class='tel'>"+
-                    "<i>"+"</i>"+
-                    "<span title=''>"+response[i]['tel']+"</span>"+
-                    "</p>"+
-                    "<p class='addr'>"+
-                    "<i>"+"</i>"+
-                    "<span title=''>"+response[i]['address']+"</span>"+
-                    "</p>"+
-                    "</dd>"+
-                    "</dl>"+
-                    "<p><label>医院简介：</label>" + response[i]['intro'] + "</p>" +
-                    "</div>"+
-                    "<div class='comment' style='float:left margin-right:920px;margin-top:200'>"+
-                    "累计 <label>133.0万</label> 次预约<div class='pd15'> "+"</div>"+
-                    "累计 <label>6.1万</label> 条患者评价"+
-                    "</div>"+
-                    "</div>"+
-                    "<a class='cover-bg' target='_blank' href='http://www.guahao.com/hospital/5cee04f9-4cc8-4499-a35b-6f37f2dd8a74000'>"+"</a>"+
-                    " </li>";
+        if(xmlHttp.readyState == 4 && xmlHttp.status ==200) {
+            response = eval(xmlHttp.response);
+            for (var i = 0; i < response.length; i++) {
+                var listItem =
+                    "<li class='g-hospital-item J_hospitalItem'>" +
+                    "<div class='hos-total' style='width:100%;height:145px;border-bottom:1px solid #DBDBDB'>" +
+                    "<div class='info g-left' style='float:left'>" +
+                    "<a target='_blank' href='#' onmousedown='return _smartlog(this,'HPTP_1')' class='img' style='float:left'>" +
+                    "<img src='http://img.guahao.cn/images/hospital/hlarge_5cee04f9-4cc8-4499-a35b-6f37f2dd8a74.gif' alt='中国人民解放军总医院301医院' title='中国人民解放军总医院301医院' />" +
+                    "</a>" +
+                    "<dl style='float:left'>" +
+                    "<dt>" +
+                    "<a class='a' href='' onmousedown='return _smartlog(this,'HPTN_1')'target='blank' title='中国人民解放军总医院301医院'>"+response[i]['name']+"</a>" +
+                    "<em class='em2'>"+Grade+"</em>" +
+                    "<img src='http://img.guahao.cn/portal/img/guahao.png'>" +
+                    "</dt>" +
+                    "<dd>" +
+                    "<p class='tel'>" +
+                    "<i></i>" +
+                    "<span title='010-68182255'>"+response[i]['tel']+"</span>" +
+                    "</p>" +
+                    "<p class='addr'>" +
+                    "<i></i>" +
+                    "<span title='北京市海淀区复兴路28号'>"+response[i]['address']+"</span>" +
+                    "</p>" +
+
+                    "</dd>" +
+                    "</dl>" +
+                    "</div>" +
+                    "<div class='comment'style='float:right;margin-right:100px;margin-top:40px'>" +
+                    "累计 <label>133.0万</label> 次预约<div class='pd15'> </div>" +
+                    "累计 <label>6.1万</label> 条患者评价" +
+                    "</div>" +
+                    "</div>" +
+                    "</li>";
+
+                hospitallist.innerHTML += listItem;
             }
         }
     }
